9daedc481ce6ee0d1c3fae54c5584c95fde605ee
Jafar0032 <ajib.aiwa@mhs.mdp.ac.id>
2023-02-08 10:09:42 +0700

    Pesan Error Login dan Registrasi
    Mengubah pesan error saat login dan registrasi

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Akun dan/atau password anda salah. Silahkan coba lagi',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
