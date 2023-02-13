284823b52876e1c1ef024ae32865e2bc1199f480
Jafar0032 <ajib.aiwa@mhs.mdp.ac.id>
2023-02-13 20:10:38 +0700

    Add Layanan Success

--- resources/views/layanan/add.blade.php
+++ resources/views/layanan/add.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,24 +8,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Halaman Tambah Layanan</title>
 </head>
+
 <body>
     <hr>
     <a href="{{ route('layanan.main') }}" class="btn">Daftar Layanan</a>
-    <br><hr>
+    <br>
+    <hr>
 
     <h2>Form Add Layanan</h2>
     @if (session()->has('info'))
-        <div class="alert alert-success">
-            {{ session()->get('info') }}
-        </div>
+    <div class="alert alert-success">
+        {{ session()->get('info') }}
+    </div>
     @endif
     <form action="{{ url('daftarLayanan/add') }}" method="post" enctype="multipart/form-data">
-    @csrf
+        @csrf
         <div class="form-group">
             <label for="nama_layanan">Nama Layanan</label>
             <input type="text" name="nama_layanan" id="nama_layanan" placeholder="Masukkan Nama Layanan" class="form-control my-2" value="{{ old('nama_layanan') }}">
             @error('nama_layanan')
-                <div class="text-danger">{{ $message }}</div>
+            <div class="text-danger">{{ $message }}</div>
             @enderror
         </div>
 
@@ -32,7 +35,7 @@
             <label for="deskripsi">deskripsi</label>
             <input type="text" name="deskripsi" id="deskripsi" placeholder="Masukkan deskripsi" class="form-control my-2" value="{{ old('deskripsi') }}">
             @error('deskripsi')
-                <div class="text-danger">{{ $message }}</div>
+            <div class="text-danger">{{ $message }}</div>
             @enderror
         </div>
 
@@ -40,14 +43,33 @@
             <label for="id_status_jasa">jasa yang tersedia</label><br>
             @foreach($statusjasa as $item)
             @if($item->status !== "Pasien" && $item->status !== "Admin")
-                <input type="checkbox" name="jasa[]" value="{{ $item->id }}"/>  {{ $item->status }}  <input type="integer" name="harga[]" placeholder="Masukkan harga" value="{{ old('harga') }}"><br>
-            @endif                
+            <input type="checkbox" name="jasa[]" value="{{ $item->id }}" id="jasa{{ $item->id }}" onclick="show('{{ $item->id }}')" /> {{ $item->status }}
+            <div class="harga">
+                <input type="integer" style="display: none;" name="harga[]" id="harga{{ $item->id }}" placeholder="Masukkan harga" value="{{ old('harga') }}"><br>
+            </div>
+            @endif
             @endforeach
         </div>
 
         <button type="submit" class="btn btn-success mt-3" id="pesan-btn">Simpan</button>
     </form>
     <hr>
-    
+
 </body>
+
 </html>
+
+<script>
+    function show(id) {
+        var jasa = document.getElementById("jasa"+id)
+        var harga = document.getElementById("harga"+id)
+        if(jasa.checked == true)
+        {
+            harga.style.display = "block"
+        }
+        else if(jasa.checked == false)
+        {
+            harga.style.display = "none"
+        }
+    }
+</script>
