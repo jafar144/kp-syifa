faa93ddc0364284e8d7a2f4f1f2204ca0d8f8c9e
Zaraelkaff <fathimahazzahra@mhs.mdp.ac.id>
2023-03-04 18:49:11 +0700

    search pesanan by nama pasien (2)

--- app/Http/Controllers/PesananController.php
+++ app/Http/Controllers/PesananController.php
@@ -31,12 +31,12 @@ class PesananController extends Controller
                     $output .= '
                         <tr class="text-center montserrat-bold">                        
                             <td class="color-inti" scope="row">'.$i.'</td>
-                            <td class="color-inti">'.$item->user_pasien->NIK.'</td>
-                            <td class="color-inti nama_panjang">'.$item->user_pasien->nama.'</td>
+                            <td class="color-inti"><a href="/detailPasien/'.$item->id_pasien.'">'.$item->user_pasien->NIK.'</a></td>
+                            <td class="color-inti nama_panjang"><a href="/detailPasien/'.$item->id_pasien.'">'.$item->user_pasien->nama.'</a></td>
                             <td class="color-abu-tuo">'.$item->created_at.'</td>
-                            <td class="color-inti">'.$item->layanan->nama_layanan.'</td>
-                            <td></td>
-                            <td></td>        
+                            <td class="color-inti"><a href="/detailLayanan/'.$item->id_layanan.'">'.$item->layanan->nama_layanan.'</a></td>
+                            <td><div class="d-inline-flex status_chip">'.$item->status_layanan->status.'</div></td>
+                            <td><a href="/pesan/detail/'.$item->id.'" class="btn btn-success" id="pesan-btn">Detail</a></td>        
                         </tr>';
                 
                     $i++;
