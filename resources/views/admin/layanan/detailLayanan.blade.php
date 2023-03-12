527070744b12a2106be4e957e2720c7a7066135e
Jafar0032 <ajib.aiwa@mhs.mdp.ac.id>
2023-03-12 08:08:24 +0700

    Update Design Detail Layanan (Belum Selesai)

--- resources/views/admin/layanan/detailLayanan.blade.php
+++ resources/views/admin/layanan/detailLayanan.blade.php
@@ -1,22 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <title>Detail Layanan</title>
-</head>
-<body>
-    <hr>
-    <a href="/daftarLayanan">back</a>
-    <h1>{{ $layanan->nama_layanan }}</h1>
-    Tenaga medis : <br>
-    @foreach($harga_layanan as $value)
-    {{ $value->status_user->status }}= {{ $value->harga }} <br>
-    @endforeach
-    <hr>
-    <a href="{{ url('/daftarLayanan/updateView/'.$layanan->id) }}" class="btn btn-success" id="pesan-btn">Edit</a>
-    
-</body>
-</html>
+<x-admin-layout>
+
+    <div class="container">
+        <div class="py-5">
+
+            <!-- Header -->
+            <a href="{{ url('/daftarLayanan') }}" class="me-3 d-inline"><i class="fa-solid fa-arrow-left"></i></a>
+            <h3 class="montserrat-extra text-start text-shadow pt-4 d-inline">Detail Layanan</h3>
+            <div class="row mt-5">
+                <div class="col-lg-5 ">
+                    <div class="shadow-tipis rounded-card pt-3 pb-4 px-3 mx-2">
+                        <div class="d-flex">
+                            <div class="montserrat-extra text-start color-inti" style="font-size: larger;">Data Staff Medis</div>
+                        </div>
+                        <div class="row mt-4">
+                            <div class="col-lg-4">
+                                <div class="montserrat-bold">NIK</div>
+                                <div class="montserrat-bold mt-2">Nama</div>
+                                <div class="montserrat-bold mt-2">Jenis Kelamin</div>
+                                <div class="montserrat-bold mt-2">Status</div>
+                                <div class="montserrat-bold mt-2">Email</div>
+                                <div class="montserrat-bold mt-2">Tanggal Lahir</div>
+                                <div class="montserrat-bold mt-2">Alamat</div>
+                                <div class="montserrat-bold mt-4">Status Aktif</div>
+                            </div>
+                            <div class="col-lg-8">
+                                <div class="montserrat-extra">: &nbsp; {{ $staff->NIK }}</div>
+                                <div class="montserrat-extra mt-2">: &nbsp; {{ $staff->nama }}</div>
+                                <div class="montserrat-extra mt-2">: &nbsp; {{ $staff->getJenisKelamin($staff->jenis_kelamin) }}</div>
+                                <div class="montserrat-extra mt-2">: &nbsp; {{ $staff->status_user->status }}</div>
+                                <div class="montserrat-extra mt-2">: &nbsp; {{ $staff->email }}</div>
+                                <div class="montserrat-extra mt-2">: &nbsp; {{ $staff->getTanggal($staff->tanggal_lahir) }}</div>
+                                <div class="montserrat-extra mt-2">: &nbsp; {{ $staff->alamat }}</div>
+                                <div class="montserrat-extra mt-4 {{ $staff->is_active }}">: &nbsp; {{ $staff->status_active($staff->is_active) }}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @if($harga_layanan != null)
+            <table class="table table-borderless table-sm">
+                <thead>
+                    <tr class="d-flex mt-3">
+                        <th scope="col" class="col-md-2 col-sm-6 col-6">Tenaga Medis</th>
+                        <th scope="col" class="col-md-2 col-sm-6 col-6">Harga</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($harga_layanan as $item)
+                    <tr class="d-flex mt-2">
+                        <td scope="row" class="col-md-2 col-sm-6 col-6">{{ $item->status_user->status }}</td>
+                        <td class="col-md-2 col-sm-6 col-6">Rp @currency($item->harga)</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            @endif
+            <a href="{{ url('/daftarLayanan/updateView/'.$layanan->id) }}" class="btn btn-success" id="pesan-btn">Edit</a>
+
+        </div>
+    </div>
+
+</x-admin-layout>
