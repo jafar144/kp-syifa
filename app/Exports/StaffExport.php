9ed60d5479b0c336d3e285a8651c6a27448674fa
Zaraelkaff <fathimahazzahra@mhs.mdp.ac.id>
2023-03-12 19:53:37 +0700

    excel(2)

--- app/Exports/StaffExport.php
+++ app/Exports/StaffExport.php
@@ -12,9 +12,18 @@ use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 
 
-class StaffExport implements FromQuery, WithMapping, WithHeadings, ShouldAutoSize, WithStyles
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+use Maatwebsite\Excel\Concerns\ToModel;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
+use PhpOffice\PhpSpreadsheet\Cell\DefaultValueBinder;
+
+class StaffExport extends DefaultValueBinder implements WithCustomValueBinder, FromQuery, WithMapping, WithHeadings, ShouldAutoSize, WithStyles, WithColumnFormatting
 {
     use Exportable;
     /**
@@ -24,9 +33,9 @@ class StaffExport implements FromQuery, WithMapping, WithHeadings, ShouldAutoSiz
     // {
     //     $this->id = $id;
     // }
-    
     public function query()
     {
+        // return Users::query()->where('status', 'N');
         // return Users::query()->where('status', $this->id);
         return Users::query()->where('status', '!=', 'P')->where('status', '!=', 'A');
     }
@@ -34,14 +43,71 @@ class StaffExport implements FromQuery, WithMapping, WithHeadings, ShouldAutoSiz
     {
         return [
             $staff->NIK,
-            $staff->status_user->status
+            $staff->nama,
+            $staff->jenis_kelamin,
+            $staff->status_user->status,
+            $staff->email,
+            $staff->tanggal_lahir,
+            $staff->alamat,
+            $staff->notelp,
+            $staff->is_active
         ];
-    }
+    }    
     public function headings(): array
     {
         return [
             'NIK',
-            'Status'
+            'Nama',
+            'jk',
+            'Status',
+            'Email',
+            'TTL',
+            'alamat',
+            'notelp',
+            'aktif'
+        ];
+    }
+    public function bindValue(Cell $cell, $staff)
+    {
+        // if (is_numeric($staff->NIK)) {
+        //     $cell->setValueExplicit($staff, DataType::TYPE_STRING);
+
+        //     return true;
+        // }
+        if ($cell->getColumn() != 'F') {
+            $cell->setValueExplicit($staff, DataType::TYPE_STRING);
+
+            return true;
+        }
+        if ($cell->getRow() == 1) {
+            $cell->setValueExplicit($staff, DataType::TYPE_STRING);
+
+            return true;
+        }
+        if ($cell->getColumn() == 'F' && $cell->getRow() != 1) {
+            $staff = date('d/m/Y',strtotime($staff));
+            $cell->setValueExplicit($staff);
+
+            return true;
+        }
+
+        // else return default behavior
+        return parent::bindValue($cell, $staff);
+    }
+    
+    public function columnFormats(): array
+    {
+        return [
+            'A' => NumberFormat::FORMAT_TEXT,
+            'B' => NumberFormat::FORMAT_TEXT,
+            'C' => NumberFormat::FORMAT_TEXT,
+            'D' => NumberFormat::FORMAT_TEXT,
+            'E' => NumberFormat::FORMAT_TEXT,
+            'F' => NumberFormat::FORMAT_DATE_DDMMYYYY,
+            'G' => NumberFormat::FORMAT_TEXT,
+            'H' => NumberFormat::FORMAT_TEXT,
+            'I' => NumberFormat::FORMAT_TEXT
+            
         ];
     }
     public function styles(Worksheet $sheet)
