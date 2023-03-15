55c3151eedd0a6d243aab0d796bdc758c9bc53e8
Jafar0032 <ajib.aiwa@mhs.mdp.ac.id>
2023-03-15 08:28:00 +0700

    Update Posisi Modal Logout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -86,7 +86,7 @@
     </div>
     <!-- Modal -->
     <div class="modal fade" id="modalKonfirmasiLogout" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">SignOut</h5>
