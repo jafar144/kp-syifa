1c4fd9331ab2d2fa7af3f8469084ef207bd446c9
Jafar0032 <ajib.aiwa@mhs.mdp.ac.id>
2023-04-04 22:26:05 +0700

    Update Distance Map

--- public/js/map.js
+++ public/js/map.js
@@ -1,120 +1,130 @@
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      mapTypeControl: false,
-      center: { lat: -33.8688, lng: 151.2195 },
-      zoom: 13,
-    });
+  const map = new google.maps.Map(document.getElementById("map"), {
+    mapTypeControl: false,
+    center: { lat: -2.976343493555306, lng: 104.77099013136277 },
+    zoom: 19,
+  });
+
+  new AutocompleteDirectionsHandler(map);
   
-    new AutocompleteDirectionsHandler(map);
+}
+
+class AutocompleteDirectionsHandler {
+  map;
+  originPlaceId;
+  destinationPlaceId;
+  travelMode;
+  directionsService;
+  directionsRenderer;
+  constructor(map) {
+    this.map = map;
+    this.originPlaceId = "";
+    this.destinationPlaceId = "ChIJvY8ryBR2Oy4Re23JEDPldI0"; // place_id klinik alsyifa
+    this.distance = "";
+    this.travelMode = google.maps.TravelMode.DRIVING;
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
+    this.directionsRenderer.setMap(map);
+
+    const jarak = document.getElementById("jarak");
+    const originInput = document.getElementById("origin-input");
+    // const destinationInput = document.getElementById("destination-input");
+    // const modeSelector = document.getElementById("mode-selector");
+    // Specify just the place data fields that you need.
+    const originAutocomplete = new google.maps.places.Autocomplete(
+      originInput,
+      { 
+        fields: ["place_id"],
+        componentRestrictions: { country: "id" },
+      }
+    );
+    // Specify just the place data fields that you need.
+    // const destinationAutocomplete = new google.maps.places.Autocomplete(
+    //   destinationInput,
+    //   { 
+    //     fields: ["place_id"],
+    //     componentRestrictions: { country: "id" },
+    //   }
+    // );
+
+    // this.setupClickListener(
+    //   "changemode-walking",
+    //   google.maps.TravelMode.WALKING
+    // );
+    // this.setupClickListener(
+    //   "changemode-transit",
+    //   google.maps.TravelMode.TRANSIT
+    // );
+    // this.setupClickListener(
+    //   "changemode-driving",
+    //   google.maps.TravelMode.DRIVING
+    // );
+
+    this.setupPlaceChangedListener(originAutocomplete, "ORIG");
+    // this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+    // this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
+    //   destinationInput
+    // );
+    // this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
   }
-  
-  class AutocompleteDirectionsHandler {
-    map;
-    originPlaceId;
-    destinationPlaceId;
-    travelMode;
-    directionsService;
-    directionsRenderer;
-    constructor(map) {
-      this.map = map;
-      this.originPlaceId = "";
-      this.destinationPlaceId = "";
-      this.distance = "";
-      this.travelMode = google.maps.TravelMode.WALKING;
-      this.directionsService = new google.maps.DirectionsService();
-      this.directionsRenderer = new google.maps.DirectionsRenderer();
-      this.directionsRenderer.setMap(map);
-  
-      const jarak = document.getElementById("jarak");
-      const originInput = document.getElementById("origin-input");
-      const destinationInput = document.getElementById("destination-input");
-      const modeSelector = document.getElementById("mode-selector");
-      // Specify just the place data fields that you need.
-      const originAutocomplete = new google.maps.places.Autocomplete(
-        originInput,
-        { fields: ["place_id"] }
-      );
-      // Specify just the place data fields that you need.
-      const destinationAutocomplete = new google.maps.places.Autocomplete(
-        destinationInput,
-        { fields: ["place_id"] }
-      );
-  
-      this.setupClickListener(
-        "changemode-walking",
-        google.maps.TravelMode.WALKING
-      );
-      this.setupClickListener(
-        "changemode-transit",
-        google.maps.TravelMode.TRANSIT
-      );
-      this.setupClickListener(
-        "changemode-driving",
-        google.maps.TravelMode.DRIVING
-      );
-      this.setupPlaceChangedListener(originAutocomplete, "ORIG");
-      this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
-      this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-      this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
-        destinationInput
-      );
-      this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
-    }
 
-    // Sets a listener on a radio button to change the filter type on Places
-    // Autocomplete.
-    setupClickListener(id, mode) {
-      const radioButton = document.getElementById(id);
-  
-      radioButton.addEventListener("click", () => {
-        this.travelMode = mode;
-        this.route();
-      });
-    }
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  // setupClickListener(id, mode) {
+  //   const radioButton = document.getElementById(id);
 
-    setupPlaceChangedListener(autocomplete, mode) {
-      autocomplete.bindTo("bounds", this.map);
-      autocomplete.addListener("place_changed", () => {
-        const place = autocomplete.getPlace();
-  
-        if (!place.place_id) {
-          window.alert("Please select an option from the dropdown list.");
-          return;
-        }
-  
-        if (mode === "ORIG") {
-          this.originPlaceId = place.place_id;
-        } else {
-          this.destinationPlaceId = place.place_id;
-        }
-  
-        this.route();
-      });
-    }
+  //   radioButton.addEventListener("click", () => {
+  //     this.travelMode = mode;
+  //     this.route();
+  //   });
+  // }
 
-    route() {
-      if (!this.originPlaceId || !this.destinationPlaceId) {
+  setupPlaceChangedListener(autocomplete, mode) {
+    autocomplete.bindTo("bounds", this.map);
+    autocomplete.addListener("place_changed", () => {
+      const place = autocomplete.getPlace();
+
+      if (!place.place_id) {
+        window.alert("Please select an option from the dropdown list.");
         return;
       }
-  
-      const me = this;
-  
-      this.directionsService.route(
-        {
-          origin: { placeId: this.originPlaceId },
-          destination: { placeId: this.destinationPlaceId },
-          travelMode: this.travelMode,
-        },
-        (response, status) => {
-          if (status === "OK") {
-            me.directionsRenderer.setDirections(response);
-            console.log(response.routes[0].legs[0].distance)
-          } else {
-            window.alert("Directions request failed due to " + status);
-          }
-        }
-      );
-    }
+
+      if (mode === "ORIG") {
+        this.originPlaceId = place.place_id;
+      } else {
+        this.destinationPlaceId = place.place_id;
+      }
+
+      this.route();
+    });
   }
-  
-  window.initMap = initMap;
+
+  route() {
+    if (!this.originPlaceId || !this.destinationPlaceId) {
+      return;
+    }
+
+    const me = this;
+
+    this.directionsService.route(
+      {
+        origin: { placeId: this.originPlaceId },
+        destination: { placeId: this.destinationPlaceId },
+        travelMode: this.travelMode,
+      },
+      (response, status) => {
+        if (status === "OK") {
+          // Untuk menunjukan jalur nya
+          // me.directionsRenderer.setDirections(response);
+          this.distance = response.routes[0].legs[0].distance.value;
+          jarak.innerHTML += this.distance + "meter";
+        } else {
+          window.alert("Directions request failed due to " + status);
+        }
+      }
+    );
+  }
+}
+
+window.initMap = initMap;
--- resources/views/pasien/pesanan/add.blade.php
+++ resources/views/pasien/pesanan/add.blade.php
@@ -21,9 +21,9 @@
                     <div style="display: none">
                         <input id="origin-input" class="controls" type="text" placeholder="Enter an origin location" />
 
-                        <input id="destination-input" class="controls" type="text" placeholder="Enter a destination location" />
+                        <!-- <input id="destination-input" class="controls" type="text" placeholder="Enter a destination location" /> -->
 
-                        <div id="mode-selector" class="controls">
+                        <!-- <div id="mode-selector" class="controls">
                             <input type="radio" name="type" id="changemode-walking" checked="checked" />
                             <label for="changemode-walking">Walking</label>
 
@@ -32,10 +32,11 @@
 
                             <input type="radio" name="type" id="changemode-driving" />
                             <label for="changemode-driving">Driving</label>
-                        </div>
+                        </div> -->
                     </div>
 
-                    <div id="jarak"></div>
+                    <div id="jarak">Jarak rumah pasien ke klinik : </div>
+                    <div id="hasil"></div>
 
                     <div id="map" style="height: 50%;"></div>
 
