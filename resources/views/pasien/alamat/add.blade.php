49f992de61918a143403d4cb1c737530ca40c826
Jafar0032 <ajib.aiwa@mhs.mdp.ac.id>
2023-04-26 16:08:33 +0700

    Update Design Daftar dan Tambah Pasien

--- resources/views/pasien/alamat/add.blade.php
+++ resources/views/pasien/alamat/add.blade.php
@@ -1,28 +1,56 @@
-@if (session()->has('info'))
-<div class="alert alert-success">
-    {{ session()->get('info') }}
-</div>
-@endif
+<x-inti-layout :title="'Daftar Alamat'">
 
-<form action="{{ url('/profile/alamat/add') }}" method="post" enctype="multipart/form-data">
-    
-    @csrf
-    <div class="form-group">
-        <label for="alamat">Alamat</label>
-        <input type="text" name="alamat" id="alamat" placeholder="Masukkan alamat" class="form-control my-2" value="{{ old('alamat') }}">
-        @error('alamat')
-        <div class="text-danger">{{ $message }}</div>
-        @enderror
+    <div class="container">
+
+        <div class="py-5">
+            <div class="pt-5">
+                <div class="pt-4 px-3">
+
+                    @if (session()->has('info'))
+                    <div class="alert alert-success">
+                        {{ session()->get('info') }}
+                    </div>
+                    @endif
+
+                    <div class="mt-4">
+                        @if($errors->any())
+                        {!! implode('', $errors->all('
+                        <div class="text-danger ms-3 mt-2 montserrat-extra"><i class="fa-2xs fa-sharp fa-solid fa-circle"></i> &nbsp; :message </div>
+                        ')) !!}
+                        @endif
+                    </div>
+
+                    <a href="{{ url('/profile/alamat') }}" class="me-3 d-inline"><i class="fa-solid fa-arrow-left"></i></a>
+                    <h3 class="d-inline montserrat-extra text-start">Tambah Alamat</h3>
+
+                    <form action="{{ url('/profile/alamat/add') }}" method="post" enctype="multipart/form-data" class="mt-4">
+                        @csrf
+
+                        <label for="alamat">Alamat</label>
+                        <!-- <div class="mt-4">
+                            <input id="origin-input" class="form-control" type="text" placeholder="Enter an origin location" />
+                        </div> -->
+                        <div>
+                            <input type="text" name="alamat" id="origin-input" placeholder="Masukkan alamat" class="form-control my-2">
+                        </div>
+                        <div id="map" style="display: none;"></div>
+
+                        <div class="form-group mt-4">
+                            <label for="detail">Detail Alamat / Patokan</label>
+                            <input type="text" name="detail" id="detail" placeholder="Contoh: samping indomaret, pagar biru" class="form-control my-2" value="{{ old('detail') }}">
+                        </div>
+
+
+                        <button type="submit" class="btn btn-success mt-3" id="pesan-btn">Simpan</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 
-    <div class="form-group">
-        <label for="detail">Detail alamat / patokan</label>
-        <input type="text" name="detail" id="detail" placeholder="Masukkan detail alamat" class="form-control my-2" value="{{ old('detail') }}">
-        @error('detail')
-        <div class="text-danger">{{ $message }}</div>
-        @enderror
-    </div>
-        
-
-    <button type="submit" class="btn btn-success mt-3" id="pesan-btn">Simpan</button>
-</form>
+</x-inti-layout>
+<script src="{{ asset('js/map.js') }}"></script>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhFi_DeV49ieQ_kgMtM8-YOP03wDimivM&callback=initMap&libraries=places&v=weekly" defer></script>
--- resources/views/pasien/alamat/main.blade.php
+++ resources/views/pasien/alamat/main.blade.php
@@ -1,17 +1,80 @@
-<h1>DAFTAR ALAMAT SAYA</h1>
-<a href="/profile/alamat/addView">add</a>
-@if (session()->has('info'))
-<div class="alert alert-success">
-    {{ session()->get('info') }}
-</div>
-@endif
-@foreach($alamat as $item)
-    <hr>{{$item->alamat}}
-    <br>
-    <a href="{{ url('/profile/alamat/updateView/'.$item->id) }}" >update</a>
-    <form action="{{ url('/profile/alamat/delete/'.$item->id) }}" method="post">
-        @csrf
-        <input type="hidden" name="_method" value="delete">
-        <button type="submit" class="btn btn-primary" id="hapus-btn">Hapus</button>
-    </form>
-@endforeach
+<x-inti-layout :title="'Daftar Alamat'">
+
+    <div class="container">
+
+        <div class="py-5">
+            <div class="pt-5">
+                <div class="pt-5 px-4">
+
+                    @if (session()->has('info'))
+                    <div class="alert alert-success">
+                        {{ session()->get('info') }}
+                    </div>
+                    @endif
+
+                    <a href="{{ url('/profile') }}" class="me-3 d-inline"><i class="fa-solid fa-arrow-left"></i></a>
+                    <h3 class="d-inline montserrat-extra text-start">Daftar Alamat Saya</h3>
+
+                    <div class="row mt-5">
+                        @foreach($alamat as $item)
+                        <div class="col-md-4 col-12">
+                            <div class="p-3 card border-end-0 border-start-0 border-bottom-0 bg-inti-muda">
+                                <a href="{{ url('/profile/alamat/updateView/'.$item->id) }}" class="remove-underline">
+                                    <div class="montserrat-extra font-smaller">{{ $item->alamat }}</div>
+                                    <div class="montserrat-bold color-abu-muda font-smaller">{{ $item->detail }}</div>
+                                    <div class="montserrat-bold font-smaller mt-4">Jarak Alamat ke Klinik : {{ $item->jarak }} meter</div>
+                                </a>
+                                <div class="mt-3">
+                                    <a type="button" class="btn btn-success mt-2 mb-2" id="btn-tolak-kecil" data-bs-toggle="modal" data-bs-target="#modalHapusAlamat">
+                                        Hapus
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Modal Batal Pesanan -->
+                        <form action="{{ url('/profile/alamat/delete/'.$item->id) }}" method="post" enctype="multipart/form-data">
+                            @csrf
+
+                            <div class="modal fade" id="modalHapusAlamat" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content shadow-tipis">
+                                        <div class="modal-header">
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body px-5">
+                                            <div class="text-center">
+                                                <i class="fa-solid fa-triangle-exclamation" style="color: #ee627e; font-size: 70px;"></i>
+                                            </div>
+                                            <div class="text-center montserrat-extra mt-4" style="font-size: larger;">Hapus Alamat</div>
+                                            <div class="text-center montserrat-bold mt-4 color-abu">Apakah anda ingin menghapus alamat ini?</div>
+                                        </div>
+                                        <div class="row mt-4 mb-4">
+                                            <div class="col-md-6 text-center">
+                                                <!-- Buttton Cancel -->
+                                                <button type="button" class="btn btn-secondary" id="btn-cancel-sedang" data-bs-dismiss="modal">Cancel</button>
+                                            </div>
+                                            <div class="col-md-6 text-center">
+                                                <!-- Button Hapus Alamat -->
+                                                <button type="submit" class="btn btn-primary" id="btn-tolak">Hapus</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </form>
+                        @endforeach
+                    </div>
+                    <div>
+                        <a href="{{ url('/profile/alamat/addView') }}" class="btn btn-primary me-5 mt-4" id="pesan-btn-sedang">
+                            <i class="fa-solid fa-plus fa-lg me-3"></i>Tambah Alamat
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</x-inti-layout>
