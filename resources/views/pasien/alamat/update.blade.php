e401d6d117ed9dd86e19c64087a7d400cf1a157a
Jafar0032 <ajib.aiwa@mhs.mdp.ac.id>
2023-05-01 12:36:59 +0700

    Design Update Alamat

--- resources/views/pasien/alamat/update.blade.php
+++ resources/views/pasien/alamat/update.blade.php
@@ -1,28 +1,58 @@
-@if (session()->has('info'))
-<div class="alert alert-success">
-    {{ session()->get('info') }}
-</div>
-@endif
+<x-inti-layout :title="'Update Alamat'">
 
-<form action="{{ url('/profile/alamat/update/'.$alamat->id) }}" method="post" enctype="multipart/form-data">
-    @method("PATCH")
-    @csrf
-    <div class="form-group">
-        <label for="alamat">Alamat</label>
-        <input type="text" name="alamat" id="alamat" placeholder="Masukkan alamat" class="form-control my-2" value="{{ old('alamat') ?? $alamat->alamat }}">
-        @error('alamat')
-        <div class="text-danger">{{ $message }}</div>
-        @enderror
+    <div class="container">
+
+        <a href="https://wa.me/" class="wa-float pt-2" target="_blank">
+            <div><i class="fa fa-xl fa-whatsapp my-float"></i> <span><strong> &nbsp; Hubungi Kami</strong></span></div>
+        </a>
+
+        <div class="py-5">
+            <div class="pt-5">
+                <div class="pt-4 px-3">
+
+                    <div class="mt-4">
+                        @if($errors->any())
+                        {!! implode('', $errors->all('
+                        <div class="text-danger ms-3 mt-2 montserrat-extra"><i class="fa-2xs fa-sharp fa-solid fa-circle"></i> &nbsp; :message </div>
+                        ')) !!}
+                        @endif
+                    </div>
+
+                    <a href="{{ url('/profile/alamat') }}" class="me-3 d-inline"><i class="fa-solid fa-arrow-left"></i></a>
+                    <h3 class="d-inline montserrat-extra text-start">Update Alamat</h3>
+
+                    <form action="{{ url('/profile/alamat/update/'.$alamat->id) }}" method="post" enctype="multipart/form-data">
+                        @method("PATCH")
+                        @csrf
+
+                        <label for="alamat" class="mt-4">Alamat</label>
+                        <div>
+                            <input type="text" name="alamat" id="origin-input" placeholder="Masukkan alamat" class="form-control my-2" value="{{ old('alamat') ?? $alamat->alamat }}">
+                        </div>
+                        <div id="map" style="display: none;"></div>
+
+                        <div>
+                            <span class="montserrat-bold text-start">Jarak ke Klinik (meter) : </span>
+                            <input type="text" name="jarak" id="jarak" style="border: none;" value="{{ old('jarak') ?? $alamat->jarak }}" readonly>
+                        </div>
+
+                        <div class="form-group mt-4">
+                            <label for="detail">Detail Alamat / Patokan</label>
+                            <input type="text" name="detail" id="detail" placeholder="Contoh: samping indomaret, pagar biru" class="form-control my-2" value="{{ old('detail') ?? $alamat->detail }}">
+                        </div>
+
+                        <button type="submit" class="btn btn-success mt-3" id="pesan-btn">Simpan</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 
-    <div class="form-group">
-        <label for="detail">Detail alamat / patokan</label>
-        <input type="text" name="detail" id="detail" placeholder="Masukkan detail alamat" class="form-control my-2" value="{{ old('detail') ?? $alamat->detail }}">
-        @error('detail')
-        <div class="text-danger">{{ $message }}</div>
-        @enderror
-    </div>
-        
+</x-inti-layout>
+<link rel="stylesheet" href="{{ asset('css/floatingWA.css') }}">
 
-    <button type="submit" class="btn btn-success mt-3" id="pesan-btn">edit</button>
-</form>
+<script src="{{ asset('js/map.js') }}"></script>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhFi_DeV49ieQ_kgMtM8-YOP03wDimivM&callback=initMap&libraries=places&v=weekly" defer></script>
