66e96187d70fcc2253e45954b502ebad33568050
Zaraelkaff <fathimahazzahra@mhs.mdp.ac.id>
2023-05-18 18:47:04 +0700

    perbaikan
    nyamoi hosting

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,3 +1,4 @@
+@props(['title'])
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -5,7 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ $title ?? "" }}</title>
+        <!-- Logo Website -->
+        <link rel="icon" type="image/x-icon" href="{{ asset('image/Logo_Kliniks.png') }}">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
